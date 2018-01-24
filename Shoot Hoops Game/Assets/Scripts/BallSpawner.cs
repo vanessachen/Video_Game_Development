e06c59cce3bba782a24be4cca86b0f88ed25a83c
vanessachen <vanchen@nuevaschool.org>
2018-01-24 12:45:08 -0800

    created new game

---
+++ Shoot Hoops Game/Assets/Scripts/BallSpawner.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawner : MonoBehaviour {
+	public GameObject ballPrefab;
+	public int maxFallDistance = -20;
+	public int ballSpeed = 9; 
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	Rigidbody createBall (){
+		//when game starts, make an instance of the object
+		GameObject instance = Instantiate (ballPrefab); //creates a new instance of the ballPrefab
+		Rigidbody rb = instance.GetComponent<Rigidbody> (); //get the ball's rigidbody component
+		//rb.velocity = Vector3.forward * 9.0f; //Vector3(0,0,1)
+		return rb;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			Rigidbody rb = createBall (); //create a new instance of the ball each time
+			rb.velocity = Vector3.forward * ballSpeed;
+			print ("I am moving forward");
+			//if (rb.position.y >= maxFallDistance){
+				//Destroy(gameObject);
+				//print ("Destroying ball now 2");
+
+			//}
+		} 
+		if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			Rigidbody rb = createBall ();
+			rb.velocity = Vector3.back * ballSpeed;
+			print ("I am moving backwards");
+		} 
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			Rigidbody rb = createBall ();
+			rb.velocity = Vector3.left * ballSpeed;
+			print ("I am moving left");
+		} 
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			Rigidbody rb = createBall ();
+			rb.velocity = Vector3.right * ballSpeed;
+			print ("I am moving right");
+		} 
+	}
+}
---
+++ Shoot Hoops Game/Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour {
+
+	 //Use this for initialization
+	void Start () {
+		//Transform t = GetComponent<Transform> (); //name a var t that gets the transform component of the camera
+		//transform var is a var that has already been created - basically you don't need the line above
+		//transform.rotation = Quaternion.Euler(0,90,0); //set the rotation property to turn 0 degrees in one axis, 90 in another, and 0 in the third
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//transform.rotation = transform.rotation * Quaternion.Euler (0,1,0);
+		float mouseX = Input.GetAxis("Mouse X");
+		transform.rotation = transform.rotation * Quaternion.Euler (0, mouseX, 0);
+		float mouseY = Input.GetAxis("Mouse Y"); //we just want to tilt the 'head' of the player 
+
+		Camera camera = GetComponentInChildren<Camera>(); //creating a var 'camera' of type Camera, and getting the child component of the player (which is the camera)
+		camera.transform.rotation = camera.transform.rotation * Quaternion.Euler (mouseY,0,0);//moving camera head based on camera rotation
+	}
+
+
+
+//	public GameObject ballPrefab;
+//	public Transform cameraTarget; 
+//	public int cameraDistance = -10; //or else positive will be away from the ball
+//	public float cameraLift = 1.5f; //on the y-axis
+//	public Vector3 move = new Vector3 (0.0f,0.0f,0.0f);
+//
+//	void LateUpdate () //LateUpdate is same as Update, but this function is called after all the other game objects are called
+//	{
+//		//position of the object  (camera) is now the target's position
+//		GameObject instance = Instantiate (ballPrefab); //creates a new instance of the ballPrefab
+//		Transform t = instance.GetComponent<Transform> ();
+//		move = new Vector3 (0,cameraLift,cameraDistance);
+//		transform.position = t.position + move; //vector3 controls it in 3 dimensions, so this is in reference of the ball, not at ball
+//		transform.LookAt (cameraTarget); 
+//		//this is something unity made so that it is easy for one object to look at another
+//	}
+
+}
